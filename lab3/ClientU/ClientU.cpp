28667fb2e33c3c9649ccb917a4c631a09932254a
ksesha <72476438+crinanis@users.noreply.github.com>
2022-10-20 09:40:49 +0300

    Add files via upload

---
+++ lab3/ClientU/ClientU.cpp
@@ -0,0 +1,138 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include "Winsock2.h"
+#include <chrono>
+#pragma comment(lib, "WS2_32.lib")
+
+using namespace std;
+using namespace std::chrono;
+
+string GetErrorMsgText(int code)
+{
+    string msgText;
+    switch (code)
+    {
+    case WSAEINTR: msgText = "WSAEINTR"; break;
+    case WSAEACCES: msgText = "WSAEACCES"; break;
+    case WSAEFAULT: msgText = "WSAEFAULT"; break;
+    case WSAEINVAL: msgText = "WSAEINVAL"; break;
+    case WSAEMFILE: msgText = "WSAEMFILE"; break;
+    case WSAEWOULDBLOCK:msgText = "WSAEWOULDBLOCK"; break;
+    case WSAEINPROGRESS:msgText = "WSAEINPROGRESS"; break;
+    case WSAENOTSOCK:msgText = "WSAENOCTSOCK"; break;
+    case WSAEDESTADDRREQ:msgText = "WSAEDESTADDRREQ"; break;
+    case WSAEMSGSIZE:msgText = "WSAEMSGSIZE"; break;
+    case WSAEPROTOTYPE:msgText = "WSAEPROTOTYPE"; break;
+    case WSAENOPROTOOPT:msgText = "WSAENOPROTOOPT"; break;
+    case WSAEPROTONOSUPPORT:msgText = "WSAEPROTONOSUPPORT"; break;
+    case WSAESOCKTNOSUPPORT:msgText = "WSAESOCKTNOSUPPORT"; break;
+    case WSAEOPNOTSUPP:msgText = "WSAEOPNOTSUPP"; break;
+    case WSAEPFNOSUPPORT:msgText = "WSAEPFNOSUPPORT"; break;
+    case WSAEAFNOSUPPORT:msgText = "WSAEAFNOSUPPORT"; break;
+    case WSAEADDRINUSE:msgText = "WSAEADDRINUSE"; break;
+    case WSAEADDRNOTAVAIL:msgText = "WSAEADDRNOTAVAIL"; break;
+    case WSAENETDOWN:msgText = "WSAENETDOWN"; break;
+    case WSAENETUNREACH:msgText = "WSAENETUNREACH"; break;
+    case WSAENETRESET:msgText = "WSAENETRESET"; break;
+    case WSAECONNABORTED:msgText = "WSAECONNABORTED"; break;
+    case WSAECONNRESET:msgText = "WSAECONNRESET"; break;
+    case WSAENOBUFS:msgText = "WSAENOBUFS"; break;
+    case WSAEISCONN:msgText = "WSAEISCONN"; break;
+    case WSAENOTCONN:msgText = "WSAENOTCONN"; break;
+    case WSAESHUTDOWN:msgText = "WSAESHUTDOWN"; break;
+    case WSAETIMEDOUT:msgText = "WSAETIMEDOUT"; break;
+    case WSAECONNREFUSED:msgText = "WSAECONNREFUSED"; break;
+    case WSAEHOSTDOWN:msgText = "WSAEHOSTDOWN"; break;
+    case WSAEHOSTUNREACH:msgText = "WSAEHOSTUNREACH"; break;
+    case WSAEPROCLIM:msgText = "WSAEPROCLIM"; break;
+    case WSASYSNOTREADY:msgText = "WSASYSNOTREADY"; break;
+    case WSAVERNOTSUPPORTED:msgText = "WSAVERNOTSUPPORTED"; break;
+    case WSANOTINITIALISED:msgText = "WSANOTINITIALISED"; break;
+    case WSAEDISCON:msgText = "WSAEDISCON"; break;
+    case WSATYPE_NOT_FOUND:msgText = "WSATYPE_NOT_FOUND"; break;
+    case WSAHOST_NOT_FOUND:msgText = "WSAHOST_NOT_FOUND"; break;
+    case WSATRY_AGAIN:msgText = "WSATRY_AGAIN"; break;
+    case WSANO_RECOVERY:msgText = "WSANO_RECOVERY"; break;
+    case WSANO_DATA:msgText = "WSANO_DATA"; break;
+    case WSA_INVALID_HANDLE:msgText = "WSA_INVALID_HANDLE"; break;
+    case WSA_INVALID_PARAMETER:msgText = "WSA_INVALID_PARAMETER"; break;
+    case WSA_IO_INCOMPLETE:msgText = "WSA_IO_INCOMPLETE"; break;
+    case WSA_IO_PENDING:msgText = "WSA_IO_PENDING"; break;
+    case WSA_NOT_ENOUGH_MEMORY:msgText = "WSA_NOT_ENOUGH_MEMORY"; break;
+    case WSA_OPERATION_ABORTED:msgText = "WSA_OPERATION_ABORTED"; break;
+    case WSAEINVALIDPROCTABLE:msgText = "WSAEINVALIDPROCTABLE"; break;
+    case WSAEINVALIDPROVIDER:msgText = "WSAEINVALIDPROVIDER"; break;
+    case WSAEPROVIDERFAILEDINIT:msgText = "WSAEPROVIDERFAILEDINIT"; break;
+    case WSASYSCALLFAILURE:msgText = "WSASYSCALLFAILURE"; break;
+    default: msgText = "***ERROR***"; break;
+    }
+    return msgText;
+}
+
+string SetErrorMsgText(string msgText, int code)
+{
+    return msgText + GetErrorMsgText(code);
+}
+
+int main()
+{
+    SOCKET cS;
+    WSADATA wsadata;
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsadata) != 0)
+            throw SetErrorMsgText("Startup: ", WSAGetLastError());
+        
+        if ((cS = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("Socket: ", WSAGetLastError());
+
+        SOCKADDR_IN serv;
+        serv.sin_family = AF_INET;
+        serv.sin_port = htons(2000);
+        serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        char obuf[50];
+        char ibuf[50];
+        int  lobuf;
+        int  lb = 0;
+
+        char numbBuf[4];
+        int lc = sizeof(serv);
+
+        high_resolution_clock::time_point t1 = high_resolution_clock::now();
+        for (int i = 0; i < 6000; i++)
+        {
+            _itoa(i, numbBuf, 10);
+            strcpy_s(obuf, "Hello from ClientU \0");
+            strcat_s(obuf, numbBuf);
+
+            if ((lobuf = sendto(cS, obuf, strlen(obuf) + 1, NULL, (sockaddr*)&serv, lc)) == SOCKET_ERROR)
+                throw  SetErrorMsgText("Send: ", WSAGetLastError());
+
+            cout << endl << ibuf << endl;
+        }
+        high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+        duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+        cout << "Time " << time_span.count() << " seconds." << std::endl << std::endl;
+
+        strcpy_s(obuf, "\0");
+
+        if ((lobuf = sendto(cS, obuf, strlen(obuf) + 1, NULL, (sockaddr*)&serv, lc)) == SOCKET_ERROR)
+           throw  SetErrorMsgText("Send:", WSAGetLastError());
+
+        system("PAUSE");
+
+        if (closesocket(cS) == SOCKET_ERROR)
+            throw SetErrorMsgText("CloseSocket: ", WSAGetLastError());
+        
+        if (WSACleanup() == SOCKET_ERROR)
+            throw SetErrorMsgText("Cleanup: ", WSAGetLastError());
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << errorMsgText;
+    }
+}
